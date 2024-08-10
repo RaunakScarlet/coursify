044e01329e1ee9982b1e6568606684336c6b798f
Raunak Scarlet <raunakscarlet@gmail.com>
2024-08-10 23:55:07 +0530

    course added

--- controllers/admin.js
+++ controllers/admin.js
@@ -5,7 +5,7 @@ export const create = async (req, res) => {
         const { title, description, price, duration, category, createdBy } =
             req.body;
       
-console.log(req.body)
+
         const image = req.file;
         const newCourse = await Course.create({
             title,
@@ -16,7 +16,7 @@ console.log(req.body)
             createdBy,
             image: image?.path,
         });
-        console.log(newCourse);
+        
         return res.status(201).json({
             message: "course created successfully",
             Course: newCourse,
